975b963afe042555ce1d18272a428dec84e69033
lovelymondays <arjism23@gmail.com>
2025-06-23 18:33:35 +0700

    update screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,43 @@ import Content from "./components/Content";
 
 export default function App() {
   const numberOfSections = 5;
-  return (
-    <Canvas
-      camera={{ position: [0, 0, 10], fov: 50 }}
-      shadows
-      className="w-full h-full z-10 fixed top-0 left-0"
-      // numberOfSections={numberOfSections}
-    >
-      <ScrollControls pages={numberOfSections}>
-        <Suspense fallback={null}>
-          <Lights />
 
-          <CoffeeModel
-            scale={0.4}
-            scrollBasedAnimation={true}
-            numberOfSections={numberOfSections}
-            isMultiple={false}
-          />
-          {[0, 1, 2].map((index) => (
+  return (
+    <div className="relative w-full h-screen">
+      <Canvas
+        camera={{ position: [0, 0, 10], fov: 50 }}
+        shadows
+        className="absolute top-0 left-0 w-full h-full"
+        // numberOfSections={numberOfSections}
+      >
+        <ScrollControls pages={numberOfSections}>
+          <Suspense fallback={null}>
+            <Lights />
+
             <CoffeeModel
-              key={`multiple-coffee-${index}`}
-              scale={0.3}
+              scale={0.4}
               scrollBasedAnimation={true}
               numberOfSections={numberOfSections}
-              isMultiple={true}
-              multipleIndex={index}
+              isMultiple={false}
             />
-          ))}
-        </Suspense>
-        <Scroll html style={{ width: "100%" }}>
-          <Content numberOfSections={numberOfSections} />
-        </Scroll>
-      </ScrollControls>
+            {[0, 1, 2].map((index) => (
+              <CoffeeModel
+                key={`multiple-coffee-${index}`}
+                scale={0.3}
+                scrollBasedAnimation={true}
+                numberOfSections={numberOfSections}
+                isMultiple={true}
+                multipleIndex={index}
+              />
+            ))}
+          </Suspense>
+          <Scroll html style={{ width: "100%" }}>
+            <Content numberOfSections={numberOfSections} />
+          </Scroll>
+        </ScrollControls>
 
-      <color attach="background" args={["#006041"]} />
-    </Canvas>
+        <color attach="background" args={["#006041"]} />
+      </Canvas>
+    </div>
   );
 }
